7fbf9b470d0880bab9453b770c6bd5418437562b
peanut <peanut@rp>
2018-09-22 00:45:11 -0700

    getting new user region by making oauth2 authentication use oauth state. Created a new provider that extends BattlenetExtendSocialite, which is a different project on github (lacma2011/socialiteproviders)

--- config/fish.php
+++ config/fish.php
@@ -6,7 +6,7 @@ return [
     ],
     'login' => [
         // emphasize through login links that they will login through battlenet, in case we allow logins from other places
-        'default_socialite_provider' => 'battlenet',
+        'default_socialite_provider' => 'battlenet-stateful', // '-stateful' will include region in OAuth state
     ],
     'battlenet' => [
         'regions' => [
